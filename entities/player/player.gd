96eb75478bdc22420bb328a01370568c4269a5a9
ElectricalOutlet <alexplaysgamezzz@outlook.com>
2026-08-01 17:57:48 -0700

    Make player tilt.

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -2,6 +2,7 @@
 class_name Player
 extends Area2D
 # Maybe you could shoot in only one direction
+# Shoot very fast
 
 const SPEED: float = 1
 
@@ -11,6 +12,8 @@ var velocity := Vector2.ZERO
 func _physics_process(_delta: float) -> void:
 	var direction: Vector2 = _get_direction()
 	velocity = direction * SPEED
+	# Tilt player.
+	rotation_degrees = lerp(rotation_degrees, 10.0 * direction.x, 0.1)
 
 	position += velocity
 
