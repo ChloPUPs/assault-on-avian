f4415b49594c7660180e938646be4c6699f1bdae
ElectricalOutlet <alexplaysgamezzz@outlook.com>
2026-08-05 17:17:56 -0700

    Improve code.

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -7,24 +7,22 @@ const TILT_SPEED: float = 0.15
 const TILE_DISTANCE: float = 10.0
 
 var velocity := Vector2.ZERO
-var direction := Vector2.ZERO
-var seed_scene: PackedScene = preload("res://entities/projectiles/seed/seed.tscn")
+var _seed_scene: PackedScene = preload("res://entities/projectiles/seed/seed.tscn")
 
-@onready var seed_container: Node2D = %SeedContainer
-@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var auto_fire_cooldown: Timer = %AutoFireCooldown
+@onready var _seed_container: Node2D = %SeedContainer
+@onready var _sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var _auto_fire_cooldown: Timer = %AutoFireCooldown
 
 
 func _physics_process(_delta: float) -> void:
-	direction = _get_direction()
+	var direction: Vector2 = _get_direction()
+	_animate_tilt(direction)
 	velocity = direction * SPEED
-	_animate_tilt()
-
 	position += velocity
 
 
 func _process(_delta: float) -> void:
-	if Input.is_action_pressed("shoot") and auto_fire_cooldown.is_stopped():
+	if Input.is_action_pressed("shoot") and _auto_fire_cooldown.is_stopped():
 		_shoot()
 
 
@@ -34,11 +32,11 @@ func _get_direction() -> Vector2:
 	return Vector2(x, y).normalized()
 
 
-func _animate_tilt() -> void:
-	sprite.rotation_degrees = lerp(sprite.rotation_degrees, TILE_DISTANCE * direction.x, TILT_SPEED)
+func _animate_tilt(direction: Vector2) -> void:
+	_sprite.rotation_degrees = lerp(_sprite.rotation_degrees, TILE_DISTANCE * direction.x, TILT_SPEED)
 
 
 func _shoot() -> void:
 	print("Player shot.")
-	seed_container.add_child(seed_scene.instantiate())
-	auto_fire_cooldown.start()
+	_seed_container.add_child(_seed_scene.instantiate())
+	_auto_fire_cooldown.start()
